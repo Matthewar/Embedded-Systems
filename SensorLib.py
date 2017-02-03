83a542a51f812d15d963b8d78b3405844d8a83dc
Matt Abdul-Rahim <Matthewar@users.noreply.github.com>
2017-02-03 18:16:35 +0000

    Multiple small additions and improvements.
    Replaced commented out enums with dictionaries.
    Updated code throughout class to match new dict types.
    Changed constructor to require I2C address argument.
    Changed values to hex format where appropriate.

--- SensorLib.py
+++ SensorLib.py
@@ -1,5 +1,4 @@
 from machine import I2C, Pin
-#from enum import Enum
 
 #?? Move constants to shared file
 SCL_PIN = 5
@@ -7,59 +6,58 @@ SDA_PIN = 4
 ##
 
 class SensorLib:
-    # Constants
-    ## Frequency
-    MIN_CLK_FREQ = 0
-    MAX_CLK_FREQ = 400000
-    ## Slave Addresses
-    ### ADDR SEL TERMINAL LEVEL   ADDR
-    ### GND                       0101001 = 41
-    ### Float                     0111001 = 57
-    ### VDD                       1001001 = 73
-    SLAVE_ADDR = 57 #?? Add argument to init to choose address ?? convert values to hex
-
-    # Types
+    # Dictionaries for used values
     #- Register
-    #class InternalRegister(Enum): ?? Change to dictionary for coherency - enums don't exist
-    CONTROL         = 0
-    TIMING          = 1
-    THRESLOWLOW     = 2
-    THRESLOWHIGH    = 3
-    THRESHIGHLOW    = 4
-    THRESHIGHHIGH   = 5
-    INTERRUPT       = 6
-    CRC             = 8
-    ID              = 10
-    DATA0LOW        = 12
-    DATA0HIGH       = 13
-    DATA1LOW        = 14
-    DATA1HIGH       = 15
-    #class GainMode(Enum):
-    X1  = False
-    X16 = True
+    InternalRegister = {
+        "CONTROL"        : 0x0,
+        "TIMING"         : 0x1,
+        "THRESLOWLOW"    : 0x2,
+        "THRESLOWHIGH"   : 0x3,
+        "THRESHIGHLOW"   : 0x4,
+        "THRESHIGHHIGH"  : 0x5,
+        "INTERRUPT"      : 0x6,
+        "CRC"            : 0x8,
+        "ID"             : 0xA,
+        "DATA0LOW"       : 0xC,
+        "DATA0HIGH"      : 0xD,
+        "DATA1LOW"       : 0xE,
+        "DATA1HIGH"      : 0xF
+    }
+    #- Gain Modes
+    GainMode = {
+        "X1"    : False,
+        "X16"   : True
+    }
+    #- Possible I2C addresses (depending on connection of ADDR pin)
+    SlaveAddrs = {
+        "GND"   : 0x29, #0101001/49
+        "FLOAT" : 0x39, #0111001/57
+        "VDD"   : 0x49  #1001001/73
+    }
 
-    def __init__(self):
+    def __init__(self,addr="FLOAT"): #?? Pass I2C to init from master controller (so can use multiple slaves on I2C bus)
         #Initialise I2C class
         self.i2c = I2C(-1, Pin(SCL_PIN), Pin(SDA_PIN))
-        #?? Set address of slave device
+        #Set Slave Address
+        self.__SlaveAddr = SensorLib.SlaveAddrs[addr]
         # Internal State
         self.__DeviceOn = True
         self.__RegTiming = bytearray(0)
 
     def __WriteData(self,Reg,Data):
-        self.i2c.writeto_mem(SensorLib.SLAVE_ADDR, Reg, Data) #?? Check ACKs received and resend if necessary ?? Need to convert Data to bytes
+        self.i2c.writeto_mem(self.__SlaveAddr, Reg, Data) #?? Check ACKs received and resend if necessary ?? Need to convert Data to bytes
 
     def __ReadData(self,Reg):
-        return self.i2c.readfrom_mem(SensorLib.SLAVE_ADDR, Reg, 1)[0]
+        return self.i2c.readfrom_mem(self.__SlaveAddr, Reg, 1)[0]
 
     def PowerUp(self,Force=False): #?? Return values for these, include check for success?
         if not self.__DeviceOn or Force:
-            __WriteData(SensorLib.CONTROL,3)
+            __WriteData(SensorLib.InternalRegister["CONTROL"],0x03)
             self.__DeviceOn = True
 
     def PowerDown(self,Force=False): #?? Return values for these, include check for success?
         if self.__DeviceOn or Force:
-            self.__WriteData(SensorLib.CONTROL,0)
+            self.__WriteData(SensorLib.InternalRegister["CONTROL"],0x00)
             self.__DeviceOn = False
 
     def SetGainMode(self,Mode):
@@ -67,10 +65,10 @@ class SensorLib:
             self.__RegTiming[4] = 1
         else:
             self.__RegTiming[4] = 0
-        __WriteData(SensorLib.TIMING,self.__RegTiming)
+        __WriteData(SensorLib.InternalRegister["TIMING"],self.__RegTiming)
 
     def GetGainMode(self):
-        return self.__ReadData(SensorLib.TIMING)[4]
+        return self.__ReadData(SensorLib.InternalRegister["TIMING"])[4]
 
     #def ChangeTiming(TimingSetting): ?? Todo
 
@@ -79,44 +77,44 @@ class SensorLib:
             if Low > 255 or Low < 0:
                 raise Exception("Low value out of bounds")
             else:
-                __WriteData(SensorLib.THRESLOWHIGH,Low.to_bytes(2,'little')[15:8])
-                __WriteData(SensorLib.THRESLOWLOW,Low.to_bytes(2,'little')[7:0])
+                __WriteData(SensorLib.InternalRegister["THRESLOWHIGH"],Low.to_bytes(2,'little')[15:8])
+                __WriteData(SensorLib.InternalRegister["THRESLOWLOW"],Low.to_bytes(2,'little')[7:0])
         if High is not None:
             if High > 255 or High < 0:
                 raise Exception("High value out of bounds")
             else:
-                __WriteData(SensorLib.THRESHIGHHIGH,High.to_bytes(2,'little')[15:8])
-                __WriteData(SensorLib.THRESHIGHLOW,High.to_bytes(2,'little')[7:0])
+                __WriteData(SensorLib.InternalRegister["THRESHIGHHIGH"],High.to_bytes(2,'little')[15:8])
+                __WriteData(SensorLib.InternalRegister["THRESHIGHLOW"],High.to_bytes(2,'little')[7:0])
 
     def GetIntrpLowThreshold(self):
         LowThreshold        = bytearray(2)
-        LowThreshold[0]   = self.__ReadData(SensorLib.THRESLOWLOW)
-        LowThreshold[1]  = self.__ReadData(SensorLib.THRESLOWHIGH)
+        LowThreshold[0]   = self.__ReadData(SensorLib.InternalRegister["THRESLOWLOW"])
+        LowThreshold[1]  = self.__ReadData(SensorLib.InternalRegister["THRESLOWHIGH"])
         return LowThreshold
 
     def GetIntrpHighThreshold(self):
         HighThreshold        = bytearray(2)
-        HighThreshold[0]    = self.__ReadData(SensorLib.THRESHIGHLOW)
-        HighThreshold[1]    = self.__ReadData(SensorLib.THRESHIGHHIGH)
+        HighThreshold[0]    = self.__ReadData(SensorLib.InternalRegister["THRESHIGHLOW"])
+        HighThreshold[1]    = self.__ReadData(SensorLib.InternalRegister["THRESHIGHHIGH"])
         return HighThreshold
 
     #Interrupt control changing function here
     def GetPartNumber(self):
-        return self.__ReadData(SensorLib.ID)[7:4]
+        return self.__ReadData(SensorLib.InternalRegister["ID"])[7:4]
 
     def GetRevNumber(self):
-        return self.__ReadData(SensorLib.ID)[3:0]
+        return self.__ReadData(SensorLib.InternalRegister["ID"])[3:0]
 
     def ReadADC0(self): #Visible and IR
         Data    = bytearray(2)
-        Data[0] = int(self.__ReadData(SensorLib.DATA0LOW))
-        Data[1] = int(self.__ReadData(SensorLib.DATA0HIGH))
+        Data[0] = int(self.__ReadData(SensorLib.InternalRegister["DATA0LOW"]))
+        Data[1] = int(self.__ReadData(SensorLib.InternalRegister["DATA0HIGH"]))
         return Data
 
     def ReadADC1(self): #Just IR
         Data        = bytearray(2)
-        Data[0] = self.__ReadData(SensorLib.DATA1LOW)
-        Data[1] = self.__ReadData(SensorLib.DATA1HIGH)
+        Data[0] = self.__ReadData(SensorLib.InternalRegister["DATA1LOW"])
+        Data[1] = self.__ReadData(SensorLib.InternalRegister["DATA1HIGH"])
         return Data
 
 #Registers
