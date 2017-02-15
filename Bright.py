581744ec0a9f541ecc2b5053c6749e3a3ee2278a
Lamber <trewy@hotmail.com>
2017-02-15 22:22:43 +0000

    Updated syntax to use 4 spaces with '#' will be ignored, and an empty message aborts the commit.

--- Bright.py
+++ Bright.py
@@ -9,38 +9,38 @@ pLEDb = machine.Pin(13)
 pwmLEDb = machine.PWM(pLEDb)
 
 def rgbchoose(timer, level):
-	if(timer == 0):
-		pwmLEDr.freq(level)
-		pwmLEDr.duty(512)
-		pwmLEDg.freq(level)
-		pwmLEDg.duty(512)
-		pwmLEDb.freq(level)
-		pwmLEDb.duty(512)
-	else:
-		pwmLEDr.freq(0)
-		pwmLEDr.duty(512)
-		pwmLEDg.freq(0)
-		pwmLEDg.duty(512)
-		pwmLEDb.freq(level)
-		pwmLEDb.duty(512)
+    if(timer == 0):
+        pwmLEDr.freq(level)
+        pwmLEDr.duty(512)
+        pwmLEDg.freq(level)
+        pwmLEDg.duty(512)
+        pwmLEDb.freq(level)
+        pwmLEDb.duty(512)
+    else:
+        pwmLEDr.freq(0)
+        pwmLEDr.duty(512)
+        pwmLEDg.freq(0)
+        pwmLEDg.duty(512)
+        pwmLEDb.freq(level)
+        pwmLEDb.duty(512)
 
 #timer: accessing if the alarm has triggered. 0 = No. 1 = Yes.
 #sun: current level of sun.
 #level: last recorded level of sun. Defaults to 0.
 def brightness(timer,sun,level):
-	if(timer == 0,level == sun):
-		print("ON")
-	else:
-		level = float(0)
-		if(timer == 1):
-			while level < 5000:
-				print(level)
-				time.sleep(1)
-				level += 100
-				rbgchoose(timer,level)
-			print("TIMER")
-		else:
-			level = sun
-			print(level)
-			print("LIGHT")
-			rgbchoose(timer,level)
+    if(timer == 0,level == sun):
+        print("ON")
+    else:
+        level = float(0)
+        if(timer == 1):
+            while level < 5000:
+                print(level)
+                time.sleep(1)
+                level += 100
+                rbgchoose(timer,level)
+            print("TIMER")
+        else:
+            level = sun
+            print(level)
+            print("LIGHT")
+            rgbchoose(timer,level)
