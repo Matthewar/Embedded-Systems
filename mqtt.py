8804480771140e3ae3bda7acd089a765a810d1bb
Matt Abdul-Rahim <Matthewar@users.noreply.github.com>
2017-02-15 23:29:09 +0000

    Converted to class and added information.

--- mqtt.py
+++ mqtt.py
@@ -1,9 +1,15 @@
-# put this at the end of main.py
 import machine
 import json
 from umqtt.simple import MQTTClient
 
-client = MQTTClient(machine.unique_id(),'192.168.0.10')
-client.connect()
+class MQTT:
+    def __init__(self):
+        client = MQTTClient(machine.unique_id(),'192.168.0.10') #New MQTT instance
+        client.connect() #Connect to instance
+        #?? Need callback?
+        client.subscribe("esys/time") #Time topic subscribed for updating time
+        client.subscribe("esys/TBD/command") #Command topic subscribed for setting alarm
 
-client.publish()
+    def SendLux(self,lux):
+        data = json.dump({'name':'Light','Level':lux}) #Assemble JSON string with lux
+        client.publish("esys/TBD/lux",bytes(data,'utf-8')) #Send to client
