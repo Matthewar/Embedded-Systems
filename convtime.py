8107b597ea4115fcf5af478e58492adc92109875
Lamber <trewy@hotmail.com>
2017-02-16 01:13:56 +0000

    Parsing of RFC3339 format to H,M,S.

---
+++ convtime.py
@@ -0,0 +1,13 @@
+import operator
+
+def convTime(RFC):
+    #"1985-04-12T23:20:50.52Z"
+    A = RFC.split("T")
+    B = operator.itemgetter(1)(A)
+    C = B.split("Z")
+    D = operator.itemgetter(0)(C)
+    E = D.split(".")
+    F = operator.itemgetter(0)(E)
+    hours = operator.itemgetter(0)(G)
+    minutes = operator.itemgetter(1)(G)
+    seconds = operator.itemgetter(2)(G)
