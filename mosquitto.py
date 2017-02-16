2369527174fe8c7f15f9104c4361ff9ab3291763
Matt Abdul-Rahim <Matthewar@users.noreply.github.com>
2017-02-16 01:08:14 +0000

    Added data from user input

--- mosquitto.py
+++ mosquitto.py
@@ -1,6 +1,8 @@
 # pip install paho-mqtt
 import paho.mqtt.client as paho
-client = paho.Client()
-client.connect("192.168.0.10")
-client.loop_start()
-client.publish("esys/tbd/command", "string")
+def ChangeAlarmTime(hour,minute):
+    client = paho.Client()
+    client.connect("192.168.0.10")
+    client.loop_start()
+    data = json.dumps({'hour':hour,'min':minute})
+    client.publish("esys/tbd/command", data)
