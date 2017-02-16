2a338585557f0ac7dceef554f2bf4aeaf32afcd0
Lamber <trewy@hotmail.com>
2017-02-16 01:39:24 +0000

    Included year,month,day,hours,minutes,seconds.

--- convtime.py
+++ convtime.py
@@ -1,7 +1,9 @@
-
 def convTime(RFC):
     #"1985-04-12T23:20:50.52Z"
+    year = int(RFC[0:4])
+    month = int(RFC[5:7])
+    day = int(RFC[8:10])
     hours = int(RFC[11:13])
     minutes = int(RFC[14:16])
     seconds = int(RFC[17:19])
-    return hours,minutes,seconds
+    return year,month,day,hours,minutes,seconds
