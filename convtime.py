bd18a74fc33c4b8a4cb77475bae3880793e3b8c9
Lamber <trewy@hotmail.com>
2017-02-16 01:31:57 +0000

    Simplified code with sub-string method.

--- convtime.py
+++ convtime.py
@@ -1,12 +1,6 @@
-import operator
 
 def convTime(RFC):
     #"1985-04-12T23:20:50.52Z"
-    A = RFC.split("T")
-    B = operator.itemgetter(1)(A)
-    C = B.split(".")
-    D = operator.itemgetter(0)(C)
-    E = D.split(":")
-    hours = operator.itemgetter(0)(E)
-    minutes = operator.itemgetter(1)(E)
-    seconds = operator.itemgetter(2)(E)
+    hours = RFC[11:13]
+    minutes = RFC[14:16]
+    seconds = RFC[17:19]
