8dd749f87a9f628eec8a0faee734ba9540757eb7
Matt Abdul-Rahim <Matthewar@users.noreply.github.com>
2017-02-16 01:48:30 +0000

    Change to add alarm functions

--- mosquitto.py
+++ mosquitto.py
@@ -4,5 +4,11 @@ def ChangeAlarmTime(hour,minute):
     client = paho.Client()
     client.connect("192.168.0.10")
     client.loop_start()
-    data = json.dumps({'hour':hour,'min':minute})
+    data = json.dumps({'command':'alarm','hour':hour,'min':minute})
+    client.publish("esys/tbd/command", data)
+def TurnOffAlarm():
+    client = paho.Client()
+    client.connect("192.168.0.10")
+    client.loop_start()
+    data = json.dumps({'command':'alarm'})
     client.publish("esys/tbd/command", data)
