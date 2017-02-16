ea1f94887365e640dca72da8e09cb1d00c5a537e
Matt Abdul-Rahim <Matthewar@users.noreply.github.com>
2017-02-16 00:13:39 +0000

    Added MQTT functionality.
    Callback function for when message arrives and check message method

--- mqtt.py
+++ mqtt.py
@@ -3,13 +3,16 @@ import json
 from umqtt.simple import MQTTClient
 
 class MQTT:
-    def __init__(self):
+    def __init__(self,callback_function):
         client = MQTTClient(machine.unique_id(),'192.168.0.10') #New MQTT instance
         client.connect() #Connect to instance
-        #?? Need callback?
+        self.client.set_callback(callback_function)
         client.subscribe("esys/time") #Time topic subscribed for updating time
         client.subscribe("esys/TBD/command") #Command topic subscribed for setting alarm
 
     def SendLux(self,lux):
         data = json.dumps({'name':'Light','Level':lux}) #Assemble JSON string with lux
         client.publish("esys/TBD/lux",bytes(data,'utf-8')) #Send to client
+
+    def CheckMsg(self):
+        self.client.check_msg()
