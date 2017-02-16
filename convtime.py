f8ee5f3a2a5339fe72ad118f50dea0b8396f16ce
Lamber <trewy@hotmail.com>
2017-02-16 01:34:13 +0000

    Changed to int tuple.

--- convtime.py
+++ convtime.py
@@ -1,6 +1,7 @@
 
 def convTime(RFC):
     #"1985-04-12T23:20:50.52Z"
-    hours = RFC[11:13]
-    minutes = RFC[14:16]
-    seconds = RFC[17:19]
+    hours = int(RFC[11:13])
+    minutes = int(RFC[14:16])
+    seconds = int(RFC[17:19])
+    return hours,minutes,seconds
