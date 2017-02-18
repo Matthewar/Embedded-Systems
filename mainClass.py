e7e0c4f2c7d7f8f2d8626e81fb47dc1fc86e602d
Matt Abdul-Rahim <Matthewar@users.noreply.github.com>
2017-02-18 21:33:41 +0000

    General fixes and improvements.

--- mainClass.py
+++ mainClass.py
@@ -1,10 +1,9 @@
-from machine import I2C, Pin, PWM, RTC
 import ssd1306
 import time
 import machine
 import json
 
-from mqtt import *
+from mqtt import MQTT
 from TSL2561Lib import *
 #from TCS34725Lib import *
 
@@ -13,69 +12,48 @@ from TSL2561Lib import *
 # m = main()
 # m.mainLoop()
 
-# Done
-def convTime(RFC):
-    #"1985-04-12T23:20:50.52Z"
-    year = int(RFC[0:4])
-    month = int(RFC[5:7])
-    day = int(RFC[8:10])
-    hours = int(RFC[11:13])
-    minutes = int(RFC[14:16])
-    seconds = int(RFC[17:19])
-    return year,month,day,hours,minutes,seconds
-
 class main:
     def __init__(self):
+        #Startup MQTT system
         self.initMQTT()
         self.initRTC()
         self.initLED()
         self.initI2C()
         self.initSensors()
         self.initOLED()
-        self.initBuzzer()
 
-    # Done
+    #MQTT Client
     def initMQTT(self):
-        self.mqtt = MQTT(self.callbackMQTT)
+        self.mqtt = MQTT(callbackMQTT)
 
     def initRTC(self):
         self.rtc = machine.RTC()
 
-    # Done
     def updateTime(self, timeString):
         # Extract time from string
-        ext_dattim = convTime(timeString)
-        rtc.datetime((ext_dattim[0], ext_dattim[1], ext_dattim[2], 1, ext_dattim[3], ext_dattim[4],     ext_dattim[5]))
-
-    # Done
-    def callbackMQTT(self, topic, data):
-        data_dict = json.loads(data)
-        print(data_dict)
-        if topic == "esys\\time":
-            jsonTimeString = data_dict['time']
-            timeString = self.convTime(jsonTimeString)
-            self.updateTime(timeString)
-        elif topic == "esys/tbd/command":
-            if data_dict['command'] == "time":
-                self.alarmHour = int(data_dict['hour'])
-                self.alarmMin = int(data_dict['min'])
-                # Setup timer interrupt
-                # Create date time tuple - use current date and check if the time has passed already.
-                #self.rtc.alarm(self.rtc.ALARM0, (self.alarmYear, self.alarmMonth, self.alarmDay, 1,     self.alarmHour, self.alarmMin, 0))
-                #self.alarmOn()
-            elif data_dict['command'] == "alarm":
-                # Alarm off
-                self.alertsOff()
+        ext_dattim = self.convTime(timeString)
+        machine.rtc.datetime((ext_dattim[0], ext_dattim[1], ext_dattim[2], 1, ext_dattim[3], ext_dattim[4],     ext_dattim[5]))
 
     def checkAlarm(self):
         if (self.alarmHour == self.rtc.datetime()[4] and self.alarmMin == self.rtc.datetime()[5]):
             self.alarmOn()
+        elif self.lux.GetLux() > 500:
+            self.lightRamp()
 
-    # Done
+    def convTime(RFC):
+        #"1985-04-12T23:20:50.52Z"
+        year = int(RFC[0:4])
+        month = int(RFC[5:7])
+        day = int(RFC[8:10])
+        hours = int(RFC[11:13])
+        minutes = int(RFC[14:16])
+        seconds = int(RFC[17:19])
+        return year,month,day,hours,minutes,seconds
+
     def initLED(self):
-        self.pwmLEDr = machine.PWM(Pin(14))
-        self.pwmLEDg = machine.PWM(Pin(12))
-        self.pwmLEDb = machine.PWM(Pin(13))
+        self.pwmLEDr = machine.PWM(machine.Pin(14))
+        self.pwmLEDg = machine.PWM(machine.Pin(12))
+        self.pwmLEDb = machine.PWM(machine.Pin(13))
         self.pwmLEDr.freq(9000)
         self.pwmLEDg.freq(9000)
         self.pwmLEDb.freq(9000)
@@ -83,62 +61,48 @@ class main:
         self.pwmLEDg.duty(1023)
         self.pwmLEDb.duty(1023)
 
-    # Done
     def initI2C(self):
-        self.i2c = I2C(-1, Pin(5), Pin(4))
+        self.i2c = machine.I2C(-1, machine.Pin(5), machine.Pin(4))
 
-    # Done
     def initSensors(self):
         self.lux = TSL2561Lib(self.i2c)
         self.lux.PowerOn()
         #self.col = TCS34725Lib(self.i2c)
         #self.col.PowerOn()
         #self.col.EnableRGBC()
-        # Setup interrupts and thresholds
-        self.lux.SetIntrThreshold(0, 13000)
-        self.lux.SetIntrCtrlSel("LVL")
-        self.p2 = Pin(2, Pin.IN)
-        self.p2.irq(trigger=Pin.IRQ_FALLING, handler=self.lightRamp)
-        #self.rtc.irq(trigger=RTC.ALARM0, handler=self.alarmOn, wake=machine.IDLE)
+####################################################################################################
+        #Chip interrupt should work this way but seems to trigger immediately on run instead.
+        ## Setup interrupts and thresholds
+        #self.lux.SetIntrThreshold(0, 13000)
+        #self.lux.SetIntrCtrlSel("LVL")
+        #self.p2 = machine.Pin(2, machine.Pin.IN)
+        #self.p2.irq(trigger=machine.Pin.IRQ_FALLING, handler=self.lightRamp)
+####################################################################################################
 
-    # Done
     def initOLED(self):
-        self.oled_reset = Pin(15, Pin.OUT, None)
+        self.oled_reset = machine.Pin(15, machine.Pin.OUT, None)
         self.oled_reset.value(0)
         time.sleep_ms(200)
         self.oled_reset.value(1)
         self.oled = ssd1306.SSD1306_I2C(128, 64, self.i2c, 0x3D)
 
-    # Done
     def lightRamp(self,p): # Gradually increase light level
         self.pwmLEDr.duty(0)
         self.pwmLEDg.duty(0)
         self.pwmLEDb.duty(0)
 
-    # Done
     def alarmOn(self):
-        self.buzz.duty(512)
         # LED on stuff
         self.pwmLEDr.duty(0)
         self.pwmLEDg.duty(0)
         self.pwmLEDb.duty(0)
 
-    # Done
     def alertsOff(self): # Turn off all alerts regardless of light/time trigger.
-        self.buzz.duty(0)
-        # self.rtc.cancel() FIX!!!!!!!!!!!
         # LED off stuff
         self.pwmLEDr.duty(1023)
         self.pwmLEDg.duty(1023)
         self.pwmLEDb.duty(1023)
 
-    # Done
-    def initBuzzer(self):
-        self.buzz = PWM(Pin(2))
-        self.buzz.freq(440)
-        self.buzz.duty(0)
-
-    # TODO - Loop forever
     def mainLoop(self):
         self.mqtt.CheckMsg()
         self.writeTime()
@@ -146,7 +110,6 @@ class main:
         self.checkAlarm()
         time.sleep(1)
 
-    # Done
     def writeTime(self):
         self.oled.fill(0)
         hours = str(self.rtc.datetime()[4])
@@ -161,8 +124,30 @@ class main:
         self.oled.text(hours + ':' + mins + ':' + secs, 0, 0)
         self.oled.show()
 
-# thinkspeak
-#m=main()
+m = main()
 
-#while True:
-#    m.mainLoop()
+def runPart():
+    m.mainLoop()
+
+def run():
+    while True:
+        runPart()
+
+def callbackMQTT(topic, data):
+    data_dict = json.loads(data)
+    print(data_dict)
+    if topic == "esys\\time":
+        jsonTimeString = data_dict['time']
+        #timeString = m.convTime(jsonTimeString)
+        m.updateTime(jsonTimeString)
+    elif topic == "esys/tbd/command":
+        if data_dict['command'] == "time":
+            m.alarmHour = int(data_dict['hour'])
+            m.alarmMin = int(data_dict['min'])
+            # Setup timer interrupt
+            # Create date time tuple - use current date and check if the time has passed already.
+            #self.rtc.alarm(self.rtc.ALARM0, (self.alarmYear, self.alarmMonth, self.alarmDay, 1,     self.alarmHour, self.alarmMin, 0))
+            #self.alarmOn()
+        elif data_dict['command'] == "alarm":
+            # Alarm off
+            m.alertsOff()
--- mqtt.py
+++ mqtt.py
@@ -6,72 +6,25 @@ import network
 class MQTT:
     def __init__(self,callback_function):
         self.initNetwork()
-        self.connectLocalAP()
+        self.localClient.connect() #Connect to instance
         self.localClient = MQTTClient(machine.unique_id(), '192.168.0.10') #New MQTT instance (local one)
         self.localClient.set_callback(callback_function) #Callback function for reading from MQTT
-        self.disconnectAP()
-        self.connectRemoteAP()
-        self.remoteClient = MQTTClient(machine.unique_id(), 'mqtt.thingspeak.com') #New MQTT instance (online one)
-        self.disconnectAP()
+        self.client.subscribe("esys\\time") #Time topic subscribed for updating time
+        self.client.subscribe("esys/tbd/command") #Command topic subscribed for setting alarm
 
     def initNetwork(self):
         self.ap_if = network.WLAN(network.AP_IF)
         self.ap_if.active(False)
         self.sta_if = network.WLAN(network.STA_IF)
-
-    def connectLocalAP(self):
         if not self.sta_if.isconnected():
             self.sta_if.active(True)
             self.sta_if.connect('EEERover', 'exhibition')
             while not self.sta_if.isconnected():
                 pass
 
-    def connectRemoteAP(self):
-        if not self.sta_if.isconnected():
-            self.sta_if.active(True)
-            self.sta_if.connect('android_hub_tmp', 'eesys_password')
-            while not self.sta_if.isconnected():
-                pass
-
-    def disconnectAP(self):
-        self.sta_if.disconnect()
-
-    def ConnectLocalHost(self):
-        self.localClient.connect() #Connect to instance
-
-    def DisconnectLocalHost(self):
-        self.localClient.disconnect() #Disconnect
-
-    def ConnectRemoteHost(self):
-        self.remoteClient.connect()
-
-    def DisconnectRemoteHost(self):
-        self.remoteClient.disconnect() #Disconnect
-
-    def SendLux(self,lux):
-        data = json.dumps({'name':'Light','Level':lux}) #Assemble JSON string with lux
-        self.ConnectLocalHost()
-        self.localClient.publish("esys/TBD/lux",bytes(data,'utf-8')) #Send to client
-        self.DisconnectLocalHost()
-
-    def SendRemoteData(self,lux,colour):
-        credentials="channels/{:s}/publish/{:s}".format("228193","77S4VPV3GNSU61OI")
-        data = "field1={:d}&field2\n={:d}".format(lux,colour)
-        self.connectRemoteAP()
-        self.ConnectRemoteHost()
-        self.remoteClient.publish(credentials,data)
-        self.DisconnectRemoteHost()
-        self.disconnectAP()
-
     def SendData(self,lux,colour):
-        self.SendLux(lux)
-        self.SendRemoteData(lux,colour)
+        data = json.dumps({'name':'Light','Level':lux}) #Assemble JSON string with lux
+        self.localClient.publish("esys/TBD/lux",bytes(data,'utf-8')) #Send to client
 
     def CheckMsg(self):
-        self.connectLocalAP()
-        self.ConnectLocalHost()
-        self.client.subscribe("esys\\time") #Time topic subscribed for updating time
-        self.client.subscribe("esys/TBD/command") #Command topic subscribed for setting alarm
-        #self.localClient.check_msg()
-        self.DisconnectLocalHost()
-        self.disconnectAP()
+        self.localClient.check_msg()
