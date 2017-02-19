751db3c53da04e33d49df157bd31d4a9ec051b9a
Matt Abdul-Rahim <Matthewar@users.noreply.github.com>
2017-02-19 20:35:32 +0000

    Updated flash file to remove comments first.
    Should save space on the device.

--- flash.sh
+++ flash.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+mkdir tmp
 for i in *.py; do
-	ampy -p $1 -b 115200 put $i
+   sed -e "s/\s*#.*$//" $i > tmp/$i
 done
+cd tmp
+for i in *.py; do
+   ampy -p $1 -b 115200 put $i
+done
+cd ..
+rm tmp -r
 
 ampy -p $1 -b 115200 reset
