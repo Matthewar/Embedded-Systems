296b6d4d26ae93bae378cc923f0c833b1c60f90e
Matt A-R <Matthewar@users.noreply.github.com>
2017-02-19 18:48:24 +0000

    Minor fix to JSON string
    Previously sending wrong command string (alarm instead of time).

--- mosquitto.py
+++ mosquitto.py
@@ -5,7 +5,7 @@ def ChangeAlarmTime(hour,minute):
     client = paho.Client()
     client.connect("192.168.0.10")
     client.loop_start()
-    data = json.dumps({'command':'alarm','hour':hour,'min':minute})
+    data = json.dumps({'command':'time','hour':hour,'min':minute})
     client.publish("esys/tbd/command", data)
 def TurnOffAlarm():
     client = paho.Client()
